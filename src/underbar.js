3f2aed0950defbb863b88a482f859261596a60b2
Alex Manusu <alex.manusu@gmail.com>
2015-07-22 18:57:33 +1000

    passing 4/8 of _.every tests

--- src/underbar.js
+++ src/underbar.js
@@ -188,6 +188,13 @@
   // Determine whether all of the elements match a truth test.
   _.every = function(collection, iterator) {
     // TIP: Try re-using reduce() here.
+    return _.reduce(collection, function(item){
+      if(!iterator(item)){
+        console.log(item);
+        return false;
+      } 
+    }, true);
+
   };
 
   // Determine whether any of the elements pass a truth test. If no iterator is
